e0954fcd7cf4ab547dc3c74539893c368cf6d223
Romain Barbier <romain.barbier@legrand.com>
2022-11-09 17:23:42 +0100

    removed secret lelelelelelel

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,9 @@ const getMatchIdUrl = 'https://europe.api.riotgames.com/lol/match/v5/matches/'
 const apiKey = 'RGAPI-2d34c150-344c-4e0e-8a16-d8d4e17b53fb'
 const subakiName = 'TlME TO TROLL'
 const subakiPUUID = "0dpkw5VWwvpErgYCoFXEtxILoqRycxP4z1E0A0UTI8SfvhPz7S3-j9h3koDxqNX-WwFvMQLf_f9RPQ"
-const secret = 'GO9y3gLoYiirbrtgQJWxgvPyAzZx1T95'
+const secret = process.env.secret
 const id = '1039339247917871124'
-const token = 'MTAzOTMzOTI0NzkxNzg3MTEyNA.GJEfQm.Au0eu4Digz-jfAVcZnYb8TXjbyZQM5YW_H6e7E'
+const token = process.env.BOT_TOKEN // Don't put your token in clear here !!
 const channelId = '1039306918142935060'
 
 export {
