022c5ad1e93d4b3490461b2f6e7df21d616c725f
Romain Barbier <romain.barbier@legrand.com>
2022-11-09 17:33:48 +0100

    removed riot api key

--- src/constants.js
+++ src/constants.js
@@ -2,13 +2,13 @@ const webhookUrl = "https://discord.com/api/webhooks/1039315482152550400/F4IipIJ
 const getSummonerUrl = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
 const getMatchPUUIDUrl = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"
 const getMatchIdUrl = 'https://europe.api.riotgames.com/lol/match/v5/matches/'
-const apiKey = 'RGAPI-2d34c150-344c-4e0e-8a16-d8d4e17b53fb'
 const subakiName = 'TlME TO TROLL'
 const subakiPUUID = "0dpkw5VWwvpErgYCoFXEtxILoqRycxP4z1E0A0UTI8SfvhPz7S3-j9h3koDxqNX-WwFvMQLf_f9RPQ"
-const secret = process.env.secret
 const id = '1039339247917871124'
-const token = process.env.BOT_TOKEN // Don't put your token in clear here !!
 const channelId = '1039306918142935060'
+const secret = process.env.SECRET
+const token = process.env.BOT_TOKEN // Don't put your token in clear here !!
+const apiKey = process.env.RIOT_KEY
 
 export {
     webhookUrl,
